622026c42c4c684e1a54d02538b8630b7c3ce63d
Artem <artyom.profis@gmail.com>
2024-02-23 10:51:27 -0500

    Test 2: get todo by ID

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -1,6 +1,6 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-//const { expect } = chai;
+const { expect } = chai;
 
 
 chai.use(chaiHttp); // Use the chai-http library
@@ -11,8 +11,17 @@ describe('REST API Tests', () => {
         chai.request('http://localhost:3000')
             .get('/todos/all')
             .end((err, res) => {
-                chai.expect(res).to.have.status(200);
-                chai.expect(res.body).to.be.an('array');
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.an('array');
+                done();
+            });
+    });
+    it('Should get a todo by ID', (done) => {   //
+        chai.request('http://localhost:3000')
+            .get('/todos/0')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.equal('buy apples'); //check assertion "buy apples" in the response body
                 done();
             });
     });
